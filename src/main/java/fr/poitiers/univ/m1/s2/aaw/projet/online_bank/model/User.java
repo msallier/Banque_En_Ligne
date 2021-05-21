c98d053b00345a743b9ad9c131b12eb4e9cdb530
MaximeSallier <maximesallier@gmail.com>
2021-05-22 00:28:50 +0200

    Déployement clever

--- src/main/java/fr/poitiers/univ/m1/s2/aaw/projet/online_bank/model/User.java
+++ src/main/java/fr/poitiers/univ/m1/s2/aaw/projet/online_bank/model/User.java
@@ -20,7 +20,7 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column
     private String name;
